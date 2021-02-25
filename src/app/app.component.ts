c4960c5c8a1384a251a26f4fb6d0a7c718f6e591
Christian Schweizer <christian.schweizer@swiss.com>
2021-02-25 15:11:50 +0100

    Basic listing of mocked flights

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+import { Flight } from './models/flight'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'flight-tracker';
+
+  currentDate = new Date();
+  
+  public flights: Flight[] = [
+    <Flight>{
+      flightNr: 'LX14',
+      flightDateTime: '01.03.2021',
+      destination: 'JFK',
+      origin: 'ZRH',
+      seatsAvailable: '43',
+      standbysListed: '4'
+    },
+    <Flight>{
+      flightNr: 'LX22',
+      flightDateTime: '01.03.2021',
+      destination: 'EWR',
+      origin: 'ZRH',
+      seatsAvailable: '12',
+      standbysListed: '1'
+    },
+  ];
 }
---
+++ src/app/models/flight.ts
@@ -0,0 +1,10 @@
+import { DeclarationListEmitMode } from "@angular/compiler";
+
+export class Flight {
+    flightNr: string;
+    flightDateTime: string;
+    origin: string;
+    destination: string;
+    seatsAvailable: string;
+    standbysListed: string;
+}
